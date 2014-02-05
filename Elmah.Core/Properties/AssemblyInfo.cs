d96d5a155fe536890630c80b8529764ecca353e2
Stevevaubell <vaubell@yahoo.com>
2014-02-05 15:02:32 +0000

    Initial commit of codebase

---
+++ Elmah.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ELMAH")]
+[assembly: AssemblyDescription("Error Logging Modules and Handlers (ELMAH) for ASP.NET")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ELMAH")]
+[assembly: AssemblyCopyright("Copyright (c) 2004-11, Atif Aziz. All rights reserved.")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.2.14318.0")]
+[assembly: AssemblyFileVersion("1.2.14318.2000")]
+[assembly: AssemblyConfiguration(Elmah.Core.Build.Configuration)]
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+
+[assembly: Elmah.Core.Scc("$Id: AssemblyInfo.cs 914 2011-12-18 20:01:36Z azizatif $")]
